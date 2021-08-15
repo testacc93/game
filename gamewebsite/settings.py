7bbb48ede3f2fa77ae6c3923dc82e8c122e5ef8d
ankur <ankurkum2021@gmail.com>
2021-08-15 15:07:17 +0530

    edite procfile

--- gamewebsite/settings.py
+++ gamewebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-020-#ylaqa*mch7imgh-y-u&v81ptou$b6q=h&a#*f6+2a_^97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['casinoinfo.herokuapp.com/', '127.0.0.1']
 
